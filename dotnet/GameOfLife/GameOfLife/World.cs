53bd0404fca78116b9a26ba74eda59f752011297
jlsears <jen.lauren.sears@gmail.com>
2015-10-06 20:50:09 -0500

    revised array of arrays to create world

--- dotnet/GameOfLife/GameOfLife/World.cs
+++ dotnet/GameOfLife/GameOfLife/World.cs
@@ -8,13 +8,27 @@ namespace GameOfLife
 {
     public class World
     {
-        string[] row1 = new string[4] { "1,1", "1,2", "1,3", "1,4" };
-        string[] row2 = new string[4] { "2,1", "2,2", "2,3", "2,4" };
-        string[] row3 = new string[4] { "3,1", "3,2", "3,3", "3,4" };
-        string[] row4 = new string[4] { "4,1", "4,2", "4,3", "4,4" };
+        public static void createWorld()
+        {
+            // Need to figure out what's amiss here when more awake
+            string[][] aWorld = new string[4][];
 
-        // Need to figure out what's amiss here when more awake
-        string[,] aWorld = new string[16] { row1[0], row1[1], row1[2], row1[3], row2[0], row2[1], row2[2], row2[3], row3[0], row3[1], row3[2], row3[3], row4[0], row4[1], row4[2], row4[3] };
+            aWorld[0] = new string[4] { "1,1", "1,2", "1,3", "1,4" };
+            aWorld[1] = new string[4] { "2,1", "2,2", "2,3", "2,4" };
+            aWorld[2] = new string[4] { "3,1", "3,2", "3,3", "3,4" };
+            aWorld[3] = new string[4] { "4,1", "4,2", "4,3", "4,4" };
+
+            //for (int i = 0; i < aWorld.Length; i++)
+            //{
+            //    System.Console.Write("Element({0}): ", i);
+
+            //    for (int j = 0; j < aWorld[i].Length; j++)
+            //    {
+            //        System.Console.Write("{0}{1}", aWorld[i][j], j == (aWorld[i].Length - 1) ? "" : " ");
+            //    }
+            //    System.Console.WriteLine();
+            //    System.Console.ReadLine();
+            //}
+        }
     }
-
 }
